b01d30d9a6115c0b641732a34c20bade1f23e3f9
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-14 18:40:12 +0200

    fourth-commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
-// import SimpleLightbox from "simplelightbox";
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// const galleryRef = document.querySelector('.gallery');
-// galleryAdd(galleryItems);
-// function galleryAdd (galleryArray) {
-//     const markup = galleryArray.map(image=>
-//         `<a class="gallery__item" href="${image.original}">
-//         <img class="gallery__image" 
-//         src="${image.preview}" 
-//         alt="${image.description}" />
-//       </a>`).join('');
-//       galleryRef.insertAdjacentHTML('beforeend', markup);
-// }
 
-//     let lightbox = new SimpleLightbox(".gallery a",   {
-//         captionsData: 'alt',
-//         captionPosition: 'bottom',
-//         captionDelay: 250,
-//       });
-//       lightbox.on('show.simplelightbox', function () {});
+console.log(galleryItems);
