39f1f4585f061584bc742fd412dc308c606a7289
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-14 22:37:50 +0200

    seventh-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector("iframe");
+const player = new Player (iframe);
+const key = "videoplayer-current-time";
+const local = localStorage.getItem(key);
+if (local) {
+    player.setCurrentTime(parseFloat(local));
+  }
+player.on('timeupdate', throttle ((data)=>{
+  
+  localStorage.setItem(key, data.seconds.toString());
+}, 1000)
+);
