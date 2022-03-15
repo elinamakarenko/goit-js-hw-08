d0986a5e7b055166235e6fb9369fea11b08da948
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-15 22:38:12 +0200

    eightth-commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+initForm();
 
+form.addEventListener('submit', event=>{
+    event.preventDefault();
+   const formData = new FormData(form);
+   formData.forEach((key, value) => 
+       console.log(`${value}-${key}`)
+   );
+   localStorage.removeItem('feedback-form-state');
+  form.elements.email.value = "";
+  form.elements.message.value = "";
+});
+
+form.addEventListener('input', throttle ((event)=>{
+let parsedFilters = localStorage.getItem('feedback-form-state');
+parsedFilters = parsedFilters ? JSON.parse(parsedFilters) : {};
+console.log(parsedFilters);
+parsedFilters[event.target.name]=event.target.value;
+if(parsedFilters){
+    localStorage.setItem('feedback-form-state', JSON.stringify(parsedFilters));
+}
+}, 2000)
+)
+
+function initForm() {
+    let parsedFilters = localStorage.getItem('feedback-form-state');
+    if (parsedFilters) {
+        parsedFilters = JSON.parse(parsedFilters);
+        Object.entries(parsedFilters).forEach(
+            ([name, value]) => (form.elements[name].value = value),
+        );
+    }
+ 
+   
+}
